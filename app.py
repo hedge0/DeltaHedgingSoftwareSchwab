ebeca4ad1d8bc7718e0a1f1f7618feb7a7f9468d
Elias Martin <elias@sportsvisio.com>
2024-09-20 21:26:26 -0400

    stripped more code

--- app.py
+++ app.py
@@ -1,25 +1,21 @@
 import asyncio
-from decimal import Decimal
 import os
 from dotenv import load_dotenv
-from datetime import datetime
 
 # Load environment variables from .env file
 load_dotenv()
 
 # Constants and Global Variables
 config = {}
-options_chains = {}
 
 # Global variables for Tastytrade
 session = None
-account = None
 
 async def main():
     """
     Main function to initialize the bot.
     """
-    global session, account, options_chains
+    global session
     
     load_config()
 
@@ -30,21 +26,12 @@ async def main():
     try:
         session = Session(login=config["TASTYTRADE_USERNAME"], password=config["TASTYTRADE_PASSWORD"], remember_me=True)
         print("Login successful.")
-    except TastytradeError as e:
+    except Exception as e:
         if "invalid_credentials" in str(e):
             print("Invalid login credentials. Please check your username and password.")
             return
         else:
             raise
-
-    try:
-        account = Account.get_account(session, config["TASTYTRADE_ACCOUNT_NUMBER"])
-    except TastytradeError as e:
-        if "invalid_account_number" in str(e):
-            print("Invalid account number. Please check your account number.")
-            return
-        else:
-            raise  
 
 
 
@@ -57,49 +44,7 @@ async def main():
 
         positions = account.get_positions(session)
 
-        for position in positions:
-            if position.instrument_type == InstrumentType.EQUITY and position.quantity != 0:
-                mid_price = await stream_raw_quotes(session, [position.underlying_symbol])
-                stocks[position.underlying_symbol] = {
-                    "quantity": int(position.quantity),
-                    "price": mid_price,
-                    "direction": 1 if position.quantity_direction == "Long" else -1
-                }
 
-            elif position.instrument_type == InstrumentType.EQUITY_OPTION and position.quantity != 0 and position.underlying_symbol not in options_chains:
-                chain = get_option_chain(session, position.underlying_symbol)
-                options_chains[position.underlying_symbol] = chain
-
-        for position in positions:
-            if position.instrument_type == InstrumentType.EQUITY_OPTION and position.quantity != 0:
-                if position.underlying_symbol not in options:
-                    options[position.underlying_symbol] = {}
-
-                if position.underlying_symbol not in stocks:
-                    mid_price = await stream_raw_quotes(session, [position.underlying_symbol])
-                    stocks[position.underlying_symbol] = {
-                        "quantity": 0,
-                        "price": mid_price,
-                        "direction": 1
-                    }
-
-                position_parsed = parse_option_symbol(position.symbol)
-                exp_date = datetime.strptime(position_parsed["expiration_date"], '%Y-%m-%d').date()
-                chain_data = options_chains[position.underlying_symbol][exp_date]
-
-                for option in chain_data:
-                    if option.symbol == position.symbol:
-                        position_parsed["option_type"] = "calls" if option.option_type == OptionType.CALL else "puts"
-                        position_parsed["strike"] = float(option.strike_price)
-                        position_parsed["streamer_symbol"] = option.streamer_symbol
-
-                position_parsed["price"] = 
-                position_parsed["quantity"] = float(position.quantity)
-                position_parsed["direction"] = 1 if position.quantity_direction == "Long" else -1
-
-
-                position_parsed["delta"] = ()
-                options[position.underlying_symbol][position.symbol] = position_parsed
 
         for underlying_symbol in options:
             total_deltas = 0.0
